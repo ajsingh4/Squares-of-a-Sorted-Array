f8da63f1d1b1cc0e7c2e812dd4a3b7c000c2c6b6
Aj Gill <ajgill@ucdavis.edu>
2019-04-09 15:14:10 -0700

    Initial Commit

---
+++ Squares of a Sorted Array/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Squares of a Sorted Array
+//
+//  Created by Aj Gill on 4/9/19.
+//  Copyright © 2019 Aj Gill. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
