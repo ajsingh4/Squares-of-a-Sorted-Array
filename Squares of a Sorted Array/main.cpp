f4560551b020b73fe7a40810d0bc0cad6136c6fa
Aj Gill <ajgill@ucdavis.edu>
2019-04-09 17:03:06 -0700

    Squares of a Sorted Array
    Given an array of integers A sorted in non-decreasing order, return an array of the squares of each number, also in sorted non-decreasing order.
    
    O(n^2) approach

--- Squares of a Sorted Array/main.cpp
+++ Squares of a Sorted Array/main.cpp
@@ -7,9 +7,35 @@
 //
 
 #include <iostream>
+#include <vector>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& A) {
+        vector<int> B;
+        for(int i=0; i<A.size();i++){
+            B.push_back(A[i]*A[i]);
+        }
+        for(int j=0; j<B.size(); j++){
+            for(int k=0; k<B.size(); k++){
+                if(B[j] < B[k]){
+                    int temp = B[j];
+                    B[j] = B[k];
+                    B[k] = temp;
+                }
+            }
+        }
+        return B;
+    }
+};
+
+int main() {
+    vector<int> input = {-4,-1,0,3,10};
+    vector<int> answer = Solution().sortedSquares(input);
+    for(int m=0; m<answer.size(); m++){
+        cout << answer[m] << " ";
+    }
 }
+
